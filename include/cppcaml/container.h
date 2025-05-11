ac1009b33eb74dc6917cd6fd5027a715056db399
Stefan Muenzel <source@s.muenzel.net>
2025-05-11 19:28:21 +0200

    Nullable

--- include/cppcaml/container.h
+++ include/cppcaml/container.h
@@ -5,6 +5,9 @@
 
 #include <cppcaml/conversion.h>
 
+#include <caml/memory.h>
+#include <caml/alloc.h>
+
 namespace Cppcaml
 {
 
@@ -21,6 +24,16 @@ struct FreeDeleter {
   void operator()(void* p) const {
     free(p);
   }
+};
+
+template<typename T>
+struct NullablePointer
+{
+  T* p;
+
+  NullablePointer() : p(nullptr) { }
+  NullablePointer(T* p) : p(p) { }
+  operator T*() const { return p; }
 };
 
 template<typename T, auto name, typename Deleter = FreeDeleter>
@@ -48,12 +61,57 @@ struct CamlTypeSharedPtrContainer {
 
 };
 
+template<typename T, auto name, typename Deleter = FreeDeleter>
+struct CamlTypeNullableSharedPtrContainer {
+  static constexpr const auto typename_caml = cat(name, " option");
+  static constexpr bool to_caml_allocates = true;
+
+  using CppType = NullablePointer<T>;
+  using Representative = std::shared_ptr<T>;
+  using Value = std::shared_ptr<T>;
+  using ValueExtraParameters = std::tuple<>;
+
+  static value to_caml(const std::shared_ptr<T>& sp) {
+    if(sp == nullptr) {
+      return Val_none;
+    }
+    CAMLparam0();
+    CAMLlocal1(res_inner);
+    res_inner = SharedPtrCustomValue<T>::allocate(std::move(sp));
+    CAMLreturn(caml_alloc_some(res_inner));
+  }
+
+  static std::shared_ptr<T> to_representative(NullablePointer<T> t) {
+    if(t.p == nullptr) {
+      return nullptr;
+    } else {
+      return std::shared_ptr<T>(t, Deleter());
+    }
+  }
+
+  static NullablePointer<T> of_caml(value v) {
+    if(Is_none(v)) {
+      return NullablePointer<T>();
+    } else {
+      return SharedPtrCustomValue<T>::from_value(Some_val(v)).get();
+    }
+  }
+
+};
+
 
 template<>
 struct CamlType<int*> : CamlTypeSharedPtrContainer<int, to_array("int_ptr")> { };
 
+template<>
+struct CamlType<NullablePointer<int>> : CamlTypeNullableSharedPtrContainer<int, to_array("int_ptr")> { };
+
+
 static_assert(HasToCaml<int*>);
 static_assert(HasOfCaml<int*>);
+
+static_assert(HasToCaml<NullablePointer<int>>);
+static_assert(HasOfCaml<NullablePointer<int>>);
 
 
 }
